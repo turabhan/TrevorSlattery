7d5c267664c10740d2ad1bf6665f617a67233ea6
Turabhan <agayevturabhan@example.com>
2026-03-03 14:55:29 +0500

    First commit

---
+++ src/main/java/ru/urfu/trevorslattery/TrevorSlatteryApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.trevorslattery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrevorSlatteryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrevorSlatteryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/urfu/trevorslattery/TrevorSlatteryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.trevorslattery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrevorSlatteryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
